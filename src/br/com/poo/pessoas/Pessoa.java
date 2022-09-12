57abb0a671caa5faa39057eb63fdad94db0178e2
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-12 16:16:34 -0300

    Criando base do pessoas

---
+++ src/br/com/poo/pessoas/Pessoa.java
@@ -0,0 +1,100 @@
+package br.com.poo.pessoas;
+
+public abstract class Pessoa {
+
+	private String nome;
+	private String genero;
+	private String estadoCivil;
+	private String cpf;
+	private String telefone;
+	private String email;
+	private String dataNascimento;
+	private Integer idEndereco;
+
+	public Pessoa() {
+		super();
+	}
+
+	public Pessoa(String nome, String genero, String estadoCivil, String cpf, String telefone, String email,
+			String dataNascimento, Integer idEndereco) {
+		this.nome = nome;
+		this.genero = genero;
+		this.estadoCivil = estadoCivil;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+		this.idEndereco = idEndereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Integer getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Integer idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", genero=" + genero + ", estadoCivil="
+				+ estadoCivil + ", cpf=" + cpf + ", telefone=" + telefone + ", email=" + email + ", dataNascimento="
+				+ dataNascimento + ", idEndereco=" + idEndereco + "]";
+	}
+}
