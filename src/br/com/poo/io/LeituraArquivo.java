81d2a1ff4040da51228051e7bfcab3ac240c9771
Gabriel Martins <gabriel-pms@hotmail.com>
2022-09-13 14:57:36 -0300

    Adcionando método leitor e escritor (#8)

--- src/br/com/poo/io/LeituraArquivo.java
+++ src/br/com/poo/io/LeituraArquivo.java
@@ -1,5 +1,48 @@
 package br.com.poo.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class LeituraArquivo {
+  static final String PATH_BASIC = "./temp/";
+  static final String EXTENSION = ".txt";
+
+  public static void leitor(String path) throws IOException {
+    BufferedReader buffRead = new BufferedReader(new FileReader(PATH_BASIC + path + EXTENSION));
+    String linha = "";
+
+    while(true) {
+      linha = buffRead.readLine();  
     
+      if (linha != null) {
+        System.out.println(linha);
+      } else {
+        break;
+      }
+    }
+
+    buffRead.close();
+  }
+
+
+  public static void escritor () throws IOException {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Escreva o nome do arquivo do relatório: ");
+    String path = sc.next();
+
+
+    BufferedWriter buffWrite = new BufferedWriter(new FileWriter(PATH_BASIC + path + EXTENSION, true));
+    String linha = "";
+
+    linha = sc.next();
+    buffWrite.append(linha + "\n");
+    buffWrite.close();
+    sc.close();
+
+    
+  }
 }
