cd826a011a67a263f2e2d97caa314347f59cbdee
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-13 18:50:09 -0300

    Criando base do menu

--- src/br/com/poo/Menus/MenuPrincipal.java
+++ src/br/com/poo/Menus/MenuPrincipal.java
@@ -1,8 +1,40 @@
 package br.com.poo.Menus;
 
 import java.util.Scanner;
+import br.com.poo.sistemainterno.App;
 
 public class MenuPrincipal {
-    
+
+    App app = new App();
+    String choice;
+
     public static Scanner myObj = new Scanner(System.in);
+
+    public void menuInicio() {
+        app.linhaMenu();
+        System.out.println("\n*              [1] - Logar                  *");
+        System.out.println("*             [2] - Cadastro                *");
+        System.out.println("*              [3] - Sair                   *");
+        app.linhaMenu();
+        System.out.print("\n\n=> ");
+        choice = myObj.nextLine();
+
+        switch (choice) {
+            case "1":
+                //menuLogar();
+            break;
+            case "2":
+                //menuCadastro();
+            break;
+            case "3":
+                app.limparTela();
+                System.out.println("Saindo do sistema, tenha um bom dia");
+            break;
+            default:
+                app.limparTela();
+                System.out.println("Opção invalida, tente novamente");
+                menuInicio();
+        }
+    }
+    
 }
--- src/br/com/poo/sistemainterno/App.java
+++ src/br/com/poo/sistemainterno/App.java
@@ -1,10 +1,18 @@
 package br.com.poo.sistemainterno;
 
 public class App {
-    
+
+    final int STRAM = 45;
+
     public void limparTela() {
         for (int i = 0; i < 10; i++) {
             System.out.println();
+        }
+    }
+
+    public void linhaMenu() {
+        for (int i = 0; i < STRAM; i++) {
+            System.out.print("*");
         }
     }
 }
