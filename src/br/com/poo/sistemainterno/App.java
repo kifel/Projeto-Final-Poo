f3679333ebd84291709f14298a9d8cb31504f19d
João Victor de Mello Pereira <joaovictorm.pessoal@gmail.com>
2022-09-13 18:50:39 -0300

    Kifel branch (#14)
    * criando menu
    
    * Criando base do menu

--- src/br/com/poo/sistemainterno/App.java
+++ src/br/com/poo/sistemainterno/App.java
@@ -1,7 +1,18 @@
 package br.com.poo.sistemainterno;
 
 public class App {
-    public static void main(String[] args) {
 
+    final int STRAM = 45;
+
+    public void limparTela() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+
+    public void linhaMenu() {
+        for (int i = 0; i < STRAM; i++) {
+            System.out.print("*");
+        }
     }
 }
---
+++ src/br/com/poo/sistemainterno/SistemaInterno.java
@@ -0,0 +1,11 @@
+package br.com.poo.sistemainterno;
+
+import br.com.poo.Menus.MenuPrincipal;
+
+public class SistemaInterno {
+    public static void main(String[] args) {
+
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.menuInicio();
+    }
+}
