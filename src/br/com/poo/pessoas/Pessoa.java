bbf0663e978f280d8907291593c7ad2d30ff4eb4
Davi Coelho <davidiascoelho22@gmail.com>
2022-09-14 14:08:48 -0300

    Criação do esqueleto da classe Funcionarios e Cliente

--- src/br/com/poo/pessoas/Pessoa.java
+++ src/br/com/poo/pessoas/Pessoa.java
@@ -28,55 +28,42 @@ public abstract class Pessoa {
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getGenero() {
 		return genero;
 	}
-
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
-
 	public String getEstadoCivil() {
 		return estadoCivil;
 	}
-
 	public void setEstadoCivil(String estadoCivil) {
 		this.estadoCivil = estadoCivil;
 	}
-
 	public String getCpf() {
 		return cpf;
 	}
-
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-
 	public String getTelefone() {
 		return telefone;
 	}
-
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getDataNascimento() {
 		return dataNascimento;
 	}
-
 	public void setDataNascimento(String dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
