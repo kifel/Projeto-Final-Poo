c4738638aea839d6d76d49b270e3a0bdda053f27
AmandaCasadem <110692307+AmandaCasadem@users.noreply.github.com>
2022-09-15 11:41:00 -0300

    criação de enuns (#26)

--- src/br/com/poo/enums/ContaEnum.java
+++ src/br/com/poo/enums/ContaEnum.java
@@ -1,15 +1,23 @@
 package br.com.poo.enums;
 
 public enum ContaEnum {
-    CORRENTE("corrente"), POUPANCA("poupanca");
+	CORRENTE("corrente", 1),
+	POUPANCA("poupanca", 2);
+
 
 	private final String tipoConta;
+	private final Integer idTipoConta;
 
-	ContaEnum(String tipoConta) {
+	ContaEnum(String tipoConta, Integer idTipoConta) {
 		this.tipoConta = tipoConta;
+		this.idTipoConta = idTipoConta;
+	}
+	public String getTipoConta() {
+		return tipoConta;
 	}
 
-	public String getContaEnum() {
-		return this.tipoConta;
+	public Integer getIdTipoConta() {
+		return idTipoConta;
 	}
+
 }
