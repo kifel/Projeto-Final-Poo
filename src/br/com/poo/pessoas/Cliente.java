723f41e83d257ebfead7ff07004612101ae7d433
Gabriel Martins <gabriel-pms@hotmail.com>
2022-09-15 21:59:21 -0300

    Gabriel branch (#34)
    * Modificando método leitor, adcionando map no cliente e adcionando informações no txt

--- src/br/com/poo/pessoas/Cliente.java
+++ src/br/com/poo/pessoas/Cliente.java
@@ -1,32 +1,37 @@
 package br.com.poo.pessoas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cliente extends Pessoa {
-    String senha;
-    int agencia;
-    
-    
+	String senha;
+	int agencia;
+	public static Map<String, Cliente> mapaCliente = new HashMap<>();
+
 	public Cliente() {
 		super();
 	}
-	
+
 	public Cliente(String senha, int agencia, String nome, String genero, String estadoCivil,
 			String cpf, String telefone, String email, String dataNascimento) {
 		super(nome, genero, estadoCivil, cpf, telefone, email, dataNascimento);
-		
+
 		this.senha = senha;
 		this.agencia = agencia;
 	}
-	
-	
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public int getAgencia() {
 		return agencia;
 	}
+
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
